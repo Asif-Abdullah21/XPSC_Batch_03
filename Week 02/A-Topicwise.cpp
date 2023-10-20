2dfd313945e558af91541e7e540ceaf37d8bfbc8
Asif Abdullah <129539520+Asif-Abdullah21@users.noreply.github.com>
2023-10-21 00:06:54 +0600

    Update A-Topicwise.cpp

--- Week 02/A-Topicwise.cpp
+++ Week 02/A-Topicwise.cpp
@@ -233,3 +233,58 @@ int longestSubstrDistinctChars (string s)
 
 
 */
+
+
+/*
+
+//3rd problem in way of pathan vai:
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+int longestSubstrDistinctChars (string S);
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        string S; cin >> S;
+
+        cout << longestSubstrDistinctChars (S) << endl;
+    }
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
+
+
+int longestSubstrDistinctChars (string s)
+{
+    
+    int ans =0;
+    int i=0,j=0;
+    int freq[26] = {0};
+    int n = s.size();
+    
+    while(j<n)
+    {
+        if(freq[s[j]-'a']==1)
+        {
+            while(true)
+            {
+                freq[s[i]-'a']=0;
+                i++;
+                if(s[i-1]==s[j]) break;
+            }
+        }
+        freq[s[j]-'a'] = 1;
+        
+        ans = max(ans,j-i+1);
+        j++;
+    }
+    
+    return ans;
+
+
+*/

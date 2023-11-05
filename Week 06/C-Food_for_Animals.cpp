2660df48b84c1bf42544a7f0b161cd20a297a50d
Asif Abdullah <129539520+Asif-Abdullah21@users.noreply.github.com>
2023-11-05 23:47:55 +0600

    Add files via upload

---
+++ Week 06/C-Food_for_Animals.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define nn (cout << "NO\n")
+#define yy (cout << "YES\n")
+#define no (cout << "No\n")
+#define yes (cout << "Yes\n")
+#define ll long long
+#define ull unsigned long long
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a / gcd(a, b)) * b)
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define mm(a, x) memset(a, x, sizeof(a))
+#define FIO                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+using namespace std;
+
+int main()
+{
+    FIO;
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int a,b,c,x,y; cin>> a >> b >>c >> x >> y;
+
+        x-=a;
+        y-=b;
+
+        x=max(x,0);
+        y=max(y,0);
+
+        if(x+y<=c) yy;
+        else nn;
+    }
+     
+    return 0;
+}

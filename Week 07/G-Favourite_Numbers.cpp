fee8b6add57af52c8d9d487928babd4458284025
Asif Abdullah <129539520+Asif-Abdullah21@users.noreply.github.com>
2023-11-11 22:41:24 +0600

    Add files via upload

---
+++ Week 07/G-Favourite_Numbers.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define nn (cout << "NO\n")
+#define yy (cout << "YES\n")
+#define no (cout << "No\n")
+#define yes (cout << "Yes\n")
+#define ll long long
+#define ull unsigned long long
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a / gcd(a, b)) * b)
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define mm(a, x) memset(a, x, sizeof(a))
+#define FIO                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+using namespace std;
+
+int main()
+{
+    FIO;
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n; cin >> n;
+
+        if(n%7==0 && (n&1)==0) cout << "Alice\n";
+        else if(n%9==0 && (n&1)!=0) cout << "Bob\n";
+        else cout << "Charlie\n";
+    }
+     
+    return 0;
+}
